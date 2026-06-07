2d49e825f86f070d09c280ac3354e77db91c7e02
Iralys Espinal <iralys.espinal@xarxatecactiva.com>
2026-06-07 16:05:57 -0400

    funcion maximo

---
+++ src/funciones.js
@@ -0,0 +1,14 @@
+function maximo(vector) 
+{
+
+    for (let i = 0; i < vector.length; i++)
+    
+   {
+    if (vector[i] > max) 
+        {
+        max = vector[i];
+        }
+    
+   }
+   console.log("el maximo es :", max);
+}
