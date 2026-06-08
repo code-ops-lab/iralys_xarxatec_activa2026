90f0b653c07fb4d10aacc5804e51237c710f3ac3
Iralys Espinal <iralys.espinal@xarxatecactiva.com>
2026-06-08 12:36:44 -0400

    numeros del 1 al 10

---
+++ src/ejer-while.js
@@ -0,0 +1,19 @@
+/* Ejercicios con WHILE
+Mostrar los números del 1 al 10 usando while.
+Mostrar los números del 10 al 1 usando while.
+Mostrar los números pares del 1 al 20.
+Sumar los números del 1 al 50.
+Pedir números al usuario hasta que introduzca un 0.
+Pedir una contraseña hasta que sea correcta.
+Contar cuántos intentos necesitó el usuario para acertar una contraseña.
+Generar un número aleatorio entre 1 y 10 y pedir al usuario que lo adivine.
+Pedir números y calcular la suma total hasta que el usuario introduzca -1.
+Pedir edades hasta que el usuario introduzca 0 y mostrar cuántas edades se introdujeron. */
+
+let numero=0;
+while (numero < 10) 
+    {
+     numero++;
+    console.log("los numeros son ", numero);
+    
+}
