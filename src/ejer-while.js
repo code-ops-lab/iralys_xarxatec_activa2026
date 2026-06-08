4be18e7d85b860a479e4b07469401ad4e1313ea1
Iralys Espinal <iralys.espinal@xarxatecactiva.com>
2026-06-08 12:54:39 -0400

    numeros pares del 1-20

--- src/ejer-while.js
+++ src/ejer-while.js
@@ -10,10 +10,33 @@ Generar un número aleatorio entre 1 y 10 y pedir al usuario que lo adivine.
 Pedir números y calcular la suma total hasta que el usuario introduzca -1.
 Pedir edades hasta que el usuario introduzca 0 y mostrar cuántas edades se introdujeron. */
 
-let numero=0;
+/* let numero=0;
 while (numero < 10) 
     {
      numero++;
     console.log("los numeros son ", numero);
     
-}
+} */
+
+   /*   let numero=11;
+    while (numero > 1 ) 
+    {
+     numero--;
+    console.log("los numeros son ", numero);
+    } */
+
+    // Mostrar los números pares del 1 al 20.
+
+  let numero=0;
+while (numero < 20) 
+    {
+     numero++;
+     if (numero%2==0) {
+      console.log("Los pares son", numero);
+        
+     }
+    
+    
+} 
+
+   
