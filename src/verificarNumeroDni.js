1b15aad452a7b4624502bd832efc49543ea2f3f6
Iralys Espinal <iralys.espinal@xarxatecactiva.com>
2026-06-27 00:01:53 -0400

    SegundoIntento

--- src/verificarNumeroDni.js
+++ src/verificarNumeroDni.js
@@ -1,123 +1,78 @@
 const prompt = require('readline-sync').question;
 
-const numeroDni=prompt("Entre el numero de su DNI: ");
+//const numeroDni=prompt("Entre el numero de su DNI: ");
 
-//const numeroDni="0590p600L";
+const numeroDni="05900600L";
+const letrasDNI = ["T","R","W","A","G","M","Y","F","P","D","X","B","N",
+                     "J","Z","S","Q","V","H","L","C","K","E"];
 
-function validarNumeroDni(numeroDni) {
 
-   const onlyLetter= numeroDni[numeroDni.length-1].toUpperCase();
-   const onlyNumbers = parseInt(numeroDni.slice(0, numeroDni.length - 1));
-   let mensaje=calcularLetraDNI(onlyNumbers,onlyLetter);
-   if (onlyNumbers < 0 || onlyNumbers > 99999999 || numeroDni.length > 9) mensaje = "El numero de su DNI no es correcto";
-   return mensaje; 
+
+function validateNumber(number) {
+ 
+  for (let i = 0; i < 8; i++) {
+    if (number[i] < "0" || number[i] > "9") return false;
     
+  }
+  return true;
 }
 
+function validateLetter(user_letter)
+ {
+  
+  for (let i = 0; i < letrasDNI.length; i++) {
+    if (user_letter == letrasDNI[i]) return true;
+  }
+  return false;
+  
+}
 
+function validateDniNumber(number) {
+    let mensaje="";
+    if (number.length != 9) return mensaje="El numero de DNI le sobran caracteres";
+    const onlyNumbers = number.slice(0, number.length - 1) ;
+    const onlyLetter = number[number.length - 1].toUpperCase();
    
-function calcularLetraDNI(numeroCorrecto,letra)
+
+    if (validateNumber(onlyNumbers) && validateLetter(onlyLetter))
+       return calculateDNIletter(onlyNumbers, onlyLetter);
+
+    return mensaje="Caracter incorrecto en su DNI";
+    }
+      
+  
+function calculateDNIletter(number, letra)
 {
- 
-  let resto= numeroCorrecto % 23;
-  // aqui tengo q coger el array de letras y buscar en ese array si el indice coincide con este resto y devolverlo
-  const letrasDNI = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"];
-  mensaje="La letra que introdujo no es la correcta";
-  if (letra == letrasDNI[resto]) mensaje= "Esta correcto el DNI";
+  let mensaje="";
+  let resto= number % 23;
+  mensaje="La letra no es correcta";
+  if (letra == letrasDNI[resto])
+  {
+  console.log(`DNI: ${number}\nLetra indicada: ${letra}\nResto de la división: ${resto}\nLetra calculada: ${letra}`);
+  mensaje= "El numero y la letra del DNI son correctos";
+  }
   return mensaje;
 
-
 }
-
-
-let resul=validarNumeroDni(numeroDni);
+let resul=validateDniNumber(numeroDni);
 console.log(resul);
 
 
 
+/* function validateDniNumber(number) {
+    
+    const isValidLength= number.length;
+    const onlyNumbers = number.slice(0, number.length - 1) ;
+    
+    const onlyLetter = number[number.length - 1].toUpperCase();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* //const numeroDni='12345678za';
-
-
-
-function validarNumeros(number){
- //tengo q validar primero q number tenga un tama;o de 9 y luego hacer el resto
-
-  let numeroDni; // declarada fuera, así sobrevive a las vueltas del bucle
-
-do {
-  numeroDni = prompt("Entre el numero de su DNI: ");
-  if (numeroDni.length != 9) {
-    console.log("El tamaño caracteres del DNI debe ser 9");
-  }
-} while (numeroDni.length != 9);
-
-
-  const onlyNumbers = number.slice(0, number.length - 1); // string, para validar longitud
-  const validNumber = parseInt(onlyNumbers);  // number, para validar rango
-  const onlyLetter = numeroDni[numeroDni.length-1].toUpperCase();
-
-  const isNumber = !isNaN(validNumber); // mejor que typeof para detectar si son dígitos válidos
-  const isValido = validNumber >= 0 && validNumber <= 99999999 && validNumber.length == 8;
- 
-  
-
-  if (!isNumber || !isValido) message="El número no es valido";
- 
-  return message;
-  
-
-  calcularLetraDNI(validNumber, letra);
-}
-   
-function calcularLetraDNI(numeroCorrecto,letra)
-{
- 
-  let resto= numeroCorrecto % 23;
-  // aqui tengo q coger el array de letras y buscar en ese array si el indice coincide con este resto y devolverlo
-  const letrasDNI = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"];
-  if (letra==letrasDNI[resto])console.log("Esta correcto el DNI");
-  else console.log("El DNI no es valido");
-  return "";
-
-
-}
-
-
-
-let resultado = validarNumeros(numeroDni);
-
-// aqui esta funcion va a preguntar en la cadena q introduzca l usuario si la ultim posicion coincide con la letra q saco el resto
-
- */
+    if (isValidLength == 9 && onlyNumbers.length == 8)
+    {
+    const intOnlyNumbers= parseInt(onlyNumbers);  
+    const isNotValid = intOnlyNumbers < 0 || intOnlyNumbers > 99999999;
+    if (isNotValid) return mensaje;
+    return calcularLetraDNI(onlyNumbers, onlyLetter);
+    }
+    return mensaje;
+    
+} */
