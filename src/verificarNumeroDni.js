bed8f95b96e904ab48e5ebc2251b3e296854f0ea
Iralys Espinal <iralys.espinal@xarxatecactiva.com>
2026-06-29 11:49:43 -0400

    tercerIntento

--- src/verificarNumeroDni.js
+++ src/verificarNumeroDni.js
@@ -1,78 +1,65 @@
-const prompt = require('readline-sync').question;
+//const prompt = require('readline-sync').question;
 
-//const numeroDni=prompt("Entre el numero de su DNI: ");
-
-const numeroDni="05900600L";
 const letrasDNI = ["T","R","W","A","G","M","Y","F","P","D","X","B","N",
                      "J","Z","S","Q","V","H","L","C","K","E"];
 
-
+//const numeroDni="";
 
 function validateNumber(number) {
- 
-  for (let i = 0; i < 8; i++) {
-    if (number[i] < "0" || number[i] > "9") return false;
-    
-  }
-  return true;
-}
 
-function validateLetter(user_letter)
+    if (number.length < 8) return false;
+      
+    
+    for (let i = 0; i < number.length; i++) {
+    const negative = (number[0]=="-" );
+    if (number[i] == " " || isNaN(number[i]) && !negative) return false;
+    }
+    return true;
+}
+ 
+
+     function validateLetter(user_letter)
  {
   
   for (let i = 0; i < letrasDNI.length; i++) {
     if (user_letter == letrasDNI[i]) return true;
   }
-  return false;
+    return false;
   
 }
+
+
+
 
 function validateDniNumber(number) {
-    let mensaje="";
-    if (number.length != 9) return mensaje="El numero de DNI le sobran caracteres";
-    const onlyNumbers = number.slice(0, number.length - 1) ;
-    const onlyLetter = number[number.length - 1].toUpperCase();
-   
 
-    if (validateNumber(onlyNumbers) && validateLetter(onlyLetter))
-       return calculateDNIletter(onlyNumbers, onlyLetter);
+   if (number == null) return "Dni Incorrecto";
+   const onlyNumbers = number.slice(0, number.length - 1) ;
+   if (!validateNumber(onlyNumbers)) return "Dni Incorrecto";
 
-    return mensaje="Caracter incorrecto en su DNI";
+   const convertInt= parseInt(onlyNumbers);
+   if (convertInt < 0 || convertInt > 99999999) return "Numero fuera de rango";
+
+   const onlyLetter = number[number.length - 1].toUpperCase();
+   if (!validateLetter(onlyLetter)) return "No se encuentra esa letra";
+
+   return calculateDNIletter(onlyNumbers,onlyLetter); 
+    
     }
-      
-  
+
+
 function calculateDNIletter(number, letra)
 {
-  let mensaje="";
+  
   let resto= number % 23;
-  mensaje="La letra no es correcta";
-  if (letra == letrasDNI[resto])
-  {
+  
+  if (letra != letrasDNI[resto]) return `DNI: ${number}\nLetra indicada: La letra ${letra} no es correcta\nResto de la división: ${resto}\nLetra calculada: ${letra}` ;
+  
   console.log(`DNI: ${number}\nLetra indicada: ${letra}\nResto de la división: ${resto}\nLetra calculada: ${letra}`);
-  mensaje= "El numero y la letra del DNI son correctos";
-  }
-  return mensaje;
-
+  return "El numero y la letra del DNI son correctos";
+  
 }
-let resul=validateDniNumber(numeroDni);
-console.log(resul);
 
 
-
-/* function validateDniNumber(number) {
-    
-    const isValidLength= number.length;
-    const onlyNumbers = number.slice(0, number.length - 1) ;
-    
-    const onlyLetter = number[number.length - 1].toUpperCase();
-
-    if (isValidLength == 9 && onlyNumbers.length == 8)
-    {
-    const intOnlyNumbers= parseInt(onlyNumbers);  
-    const isNotValid = intOnlyNumbers < 0 || intOnlyNumbers > 99999999;
-    if (isNotValid) return mensaje;
-    return calcularLetraDNI(onlyNumbers, onlyLetter);
-    }
-    return mensaje;
-    
-} */
+ 
+console.log(validateDniNumber(prompt("Entre el numero de su DNI: ")));
