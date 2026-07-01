ed14c0f2c455af2afe6ab578363e52acd7968a01
Iralys Espinal <iralys.espinal@xarxatecactiva.com>
2026-07-01 00:54:27 -0400

    perfeccionMensajesError

--- src/verificarNumeroDni.js
+++ src/verificarNumeroDni.js
@@ -1,15 +1,10 @@
-//const prompt = require('readline-sync').question;
-
-const letrasDNI = ["T","R","W","A","G","M","Y","F","P","D","X","B","N",
+const lettersDNI = ["T","R","W","A","G","M","Y","F","P","D","X","B","N",
                      "J","Z","S","Q","V","H","L","C","K","E"];
-
-//const numeroDni="";
 
 function validateNumber(number) {
 
     if (number.length < 8) return false;
-      
-    
+         
     for (let i = 0; i < number.length; i++) {
     const negative = (number[0]=="-" );
     if (number[i] == " " || isNaN(number[i]) && !negative) return false;
@@ -17,49 +12,41 @@ function validateNumber(number) {
     return true;
 }
  
-
      function validateLetter(user_letter)
  {
   
-  for (let i = 0; i < letrasDNI.length; i++) {
-    if (user_letter == letrasDNI[i]) return true;
+  for (let i = 0; i < lettersDNI.length; i++) {
+    if (user_letter == lettersDNI[i]) return true;
   }
     return false;
-  
-}
-
-
-
+  }
 
 function validateDniNumber(number) {
 
-   if (number == null) return "Dni Incorrecto";
+   if (number == null) return "Ha cancelado la entrada de datos";
    const onlyNumbers = number.slice(0, number.length - 1) ;
    if (!validateNumber(onlyNumbers)) return "Dni Incorrecto";
 
    const convertInt= parseInt(onlyNumbers);
-   if (convertInt < 0 || convertInt > 99999999) return "Numero fuera de rango";
+   if (convertInt < 0 || convertInt > 99999999) return "Número fuera de rango";
 
    const onlyLetter = number[number.length - 1].toUpperCase();
-   if (!validateLetter(onlyLetter)) return "No se encuentra esa letra";
+   if (!validateLetter(onlyLetter)) return "No ha introducido una letra";
 
    return calculateDNIletter(onlyNumbers,onlyLetter); 
     
     }
 
-
-function calculateDNIletter(number, letra)
+function calculateDNIletter(number, letter)
 {
   
-  let resto= number % 23;
+  let remainder= number % 23;
   
-  if (letra != letrasDNI[resto]) return `DNI: ${number}\nLetra indicada: La letra ${letra} no es correcta\nResto de la división: ${resto}\nLetra calculada: ${letra}` ;
+  if (letter != lettersDNI[remainder]) return `DNI: ${number}\nLetra indicada: La letra ${letter} no es correcta\nResto de la división: ${remainder}\nLetra calculada: ${letter}` ;
   
-  console.log(`DNI: ${number}\nLetra indicada: ${letra}\nResto de la división: ${resto}\nLetra calculada: ${letra}`);
-  return "El numero y la letra del DNI son correctos";
+  console.log(`DNI: ${number}\nLetra indicada: ${letter}\nResto de la división: ${remainder}\nLetra calculada: ${letter}`);
+  return "El número y la letra del DNI son correctos";
   
 }
 
-
- 
-console.log(validateDniNumber(prompt("Entre el numero de su DNI: ")));
+console.log(validateDniNumber(prompt("Entre el número de su DNI: ")));
